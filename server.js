5d047c32ebf744fc535c03a57d4a37d9dd69e2b1
salim imuzai <imuzaidynasty@gmail.com>
2021-05-11 14:40:18 +0100

    database connection setup

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const mongoose = require("mongoose")
 dotenv.config({ path: ".env" })
 const app = require("./app")
 
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connected")
+  })
+
 const PORT = process.env.PORT || 5000
 
 //starting server
